85774995f3ba952083cdf7518169cbe11fafa456
Julián Yachelini <juliyachelini@gmail.com>
2021-10-12 16:01:25 -0300

    adding navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import NavBar from './components/NavBar/NavBar';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
